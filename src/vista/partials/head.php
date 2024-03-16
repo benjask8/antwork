0174fb24c14e790c79abece15a8dab5614a26027
benjask8 <rosanojuan65@gmail.com>
2024-03-16 14:12:07 -0300

    Trabajando en el header

--- src/vista/partials/head.php
+++ src/vista/partials/head.php
@@ -7,13 +7,35 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>AntWork</title>
-  <link rel="icon" href="public/images/minlogo.png" type="image/x-icon">
+  <link rel="icon" href="public/images/mini-logo.png" type="image/x-icon">
 
+
+  <!-- Estilos css -->
   <link rel="stylesheet" href="public/css/styles.css">
+
+
+  <!-- recursos -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
   </head>
 
 <body>
-  <header>
-    <p>header</p>
+  <header class="header">
+    <div class="header-logo">
+      <a class="header-logo-a" href="?c=index&m=index"><img class="header-logo-img" src="public/images/logo.png" alt=""></a>
+    </div>
+    <div class="header-links">
+      <a href="/" class="header-links-a">Inicio</a>
+      <a href="/" class="header-links-a">Servicios</a>
+      <a href="/" class="header-links-a">Contacto</a>
+      <a href="/" class="header-links-a">Personal</a>
+      <a href="/" class="header-links-a">Preguntas Frecuentes</a>
+      <a href="/" class="header-links-a">Trabaja Con Nosotros</a>
+    </div>
+
+    <div class="header-btns">
+      <button class="header-btns-btn">
+        <span class="material-symbols-outlined">menu</span>
+      </button>
+    </div>
   </header>
   <section class="container-fluid">
